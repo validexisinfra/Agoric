12e100df293681946c10698a8b717445cfdd4097
VALIDEXIS <validexisglobal@gmail.com>
2025-05-27 22:26:10 +0300

    Update upgrademain.sh

--- upgrademain.sh
+++ upgrademain.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd $HOME
-rm -rf agoric-upgrade-18a
-git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-19
-cd agoric-upgrade-19
-git checkout agoric-upgrade-19
+rm -rf agoric-upgrade-19
+git clone https://github.com/Agoric/agoric-sdk.git agoric-upgrade-20
+cd agoric-upgrade-20
+git checkout agoric-upgrade-20
 ​
 yarn install && yarn build
 (cd packages/cosmic-swingset && make)
